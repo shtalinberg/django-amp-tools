db59e75f982b807b2816fcce8eb43222b162b8d4
shtalinberg <O.Shtalinberg@gmail.com>
2017-06-25 11:49:40 +0300

    list to tuple

--- amp_tools/templatetags/amp_tags.py
+++ amp_tools/templatetags/amp_tags.py
@@ -55,7 +55,9 @@ class AddGetParameter(Node):
 @register.tag
 def amp_link(parser, token):
     url = token.split_contents()[1:]
-    params = ["%s=%s" % (settings.AMP_TOOLS_GET_PARAMETER, settings.AMP_TOOLS_GET_VALUE)]
+    params = {
+        settings.AMP_TOOLS_GET_PARAMETER: settings.AMP_TOOLS_GET_VALUE,
+    }
     return AddGetParameter(params, url)
 
 
